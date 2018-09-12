459bc20545f2bfc161a77e45f5cf6ac2db5d9bb0
Pavel Slama <p.slama@atlas.cz>
2018-09-12 16:33:21 +0200

    font + dependencies

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 Simple Python3 script that takes a picture from CSI camera, reads HMC5883L I2C compass heading and overlay the picture. It than sends it over FTP to the server in specified interval. Useful as a meteo cam and because I have it on rotary mast with antennas, compass comes on scene.
 
-Also enable I2C bus and camera in raspi-config
+Also enable **I2C bus** and **camera** in raspi-config
 
 # How to install
 ```sh
+sudo apt install git python3-smbus python3-picamera python3-pip dos2unix libtiff5-dev libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk libopenjp2-7 -y
+sudo pip3 install pillow -I
+
 git clone git://github.com/pilotak/raspberry-python-webcam.git
 cd ./raspberry-python-webcam
-sudo apt-get install python3-smbus python3-picamera dos2unix
-sudo apt-get install libtiff5-dev libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
-sudo pip3 install pillow -I
 
 dos2unix ./meteo.py && chmod +x ./meteo.py
 ```
--- meteo.py
+++ meteo.py
@@ -14,10 +14,11 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import Image
 
-## CONFIG
+# CONFIG
 localPath = "/home/pi/raspberry-python-webcam/"
 imageFile = "image.jpg"
-font = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf",25)
+font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 25)
 
 compass_address = 0x1e
 x_offset = -82
@@ -32,14 +33,17 @@ remoteFile = "output.jpg"
 
 bus = smbus.SMBus(1)
 
+
 def read_byte(adr):
     return bus.read_byte_data(compass_address, adr)
 
+
 def read_word(adr):
     high = bus.read_byte_data(compass_address, adr)
-    low = bus.read_byte_data(compass_address, adr+1)
+    low = bus.read_byte_data(compass_address, adr + 1)
     val = (high << 8) + low
     return val
+
 
 def read_word_2c(adr):
     val = read_word(adr)
@@ -48,12 +52,14 @@ def read_word_2c(adr):
     else:
         return val
 
+
 def write_byte(adr, value):
     bus.write_byte_data(compass_address, adr, value)
 
-write_byte(0, 0b01110000) # Set to 8 samples @ 15Hz
-write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-write_byte(2, 0b00000000) # Continuous sampling
+
+write_byte(0, 0b01110000)  # Set to 8 samples @ 15Hz
+write_byte(1, 0b00100000)  # 1.3 gain LSb / Gauss 1090 (default)
+write_byte(2, 0b00000000)  # Continuous sampling
 
 scale = 0.92
 
@@ -62,7 +68,7 @@ y_out = (read_word_2c(7) - y_offset) * scale
 z_out = (read_word_2c(5)) * scale
 
 
-bearing  = math.atan2(y_out, x_out) 
+bearing = math.atan2(y_out, x_out)
 if (bearing < 0):
     bearing += 2 * math.pi
 
@@ -80,26 +86,27 @@ stream.seek(0)
 
 img = Image.open(stream)
 draw = ImageDraw.Draw(img)
-draw.text((430, 450), datetime.datetime.now().strftime("%d.%m.%Y %H:%M"),(255,255,255),font=font)
+draw.text((430, 450), datetime.datetime.now().strftime(
+    "%d.%m.%Y %H:%M"), (255, 255, 255), font=font)
 
-compass = Image.open(localPath+"compass.png")
+compass = Image.open(localPath + "compass.png")
 img.paste(compass, (555, 10), compass)
 
-needle = Image.open(localPath+"needle.png")
-needle = needle.rotate(degrees,PIL.Image.BICUBIC)
+needle = Image.open(localPath + "needle.png")
+needle = needle.rotate(degrees, PIL.Image.BICUBIC)
 needle = needle.transpose(Image.FLIP_LEFT_RIGHT)
-#needle = needle.rotate(180)
+# needle = needle.rotate(180)
 img.paste(needle, (556, 10), needle)
 
 draw = ImageDraw.Draw(img)
-img.save(localPath+imageFile, "jpeg", quality=100)
+img.save(localPath + imageFile, "jpeg", quality=100)
 
 del draw, img, compass, needle
 time.sleep(2)
 
 session = FTP(ftpHost, ftpUser, ftpPassword)
 session.cwd(remotePath)
-file = open(localPath+imageFile,'rb')
-session.storbinary('STOR '+remoteFile, file)
+file = open(localPath + imageFile, 'rb')
+session.storbinary('STOR ' + remoteFile, file)
 file.close()
 session.quit()
